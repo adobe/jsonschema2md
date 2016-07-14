cab23e315ddd07a43652884edf5c33261ec000d2
Stefan Guggisberg <sguggisb@adobe.com>
2016-07-14 18:15:40 +0200

    html generation from .n3 schema (WIP)

---
+++ constants.js
@@ -0,0 +1,46 @@
+/*************************************************************************
+ *
+ * ADOBE CONFIDENTIAL
+ * ___________________
+ *
+ *  Copyright 2016 Adobe Systems Incorporated
+ *  All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Adobe Systems Incorporated and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Adobe Systems Incorporated and its
+ * suppliers and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Adobe Systems Incorporated.
+ **************************************************************************/
+
+'use strict';
+
+var consts = {};
+
+// schema vocabulary
+
+consts.OWL_ONTOLOGY = 'http://www.w3.org/2002/07/owl#Ontology';
+consts.DC_TITLE = 'http://purl.org/dc/elements/1.1/title';
+consts.RDF_TYPE = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type';
+consts.RDFS_CLASS = 'http://www.w3.org/2000/01/rdf-schema#Class';
+consts.RDF_PROPERTY = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#Property';
+consts.RDFS_COMMENT = 'http://www.w3.org/2000/01/rdf-schema#comment';
+consts.RDFS_SUBCLASSOF = 'http://www.w3.org/2000/01/rdf-schema#subClassOf';
+consts.RDFS_ISDEFINEDBY = 'http://www.w3.org/2000/01/rdf-schema#isDefinedBy';
+consts.RDFS_DOMAIN = 'http://www.w3.org/2000/01/rdf-schema#domain';
+consts.RDFS_RANGE = 'http://www.w3.org/2000/01/rdf-schema#range';
+consts.RDFS_LABEL = 'http://www.w3.org/2000/01/rdf-schema#label';
+
+consts.SKOS_EXAMPLE = 'http://www.w3.org/2004/02/skos/core#example';
+consts.SKOS_NOTE = 'http://www.w3.org/2004/02/skos/core#note';
+
+consts.CORE_BASECLASS = 'http://ns.adobe.com/adobecloud/core/1.0/BaseClass';
+consts.CORE_BASEPROPERTY = 'http://ns.adobe.com/adobecloud/core/1.0/BaseProperty';
+consts.META_JSONNAME = 'http://ns.adobe.com/adobecloud/meta/1.0/jsonName';
+
+consts.ID = '@id'; // IRI of 'this' js object
+
+module.exports = consts;
--- index.js
+++ index.js
@@ -21,57 +21,34 @@
 var fs = require('fs');
 
 var _ = require('lodash');
-var N3 = require('n3');
+var rdf = require('node-rdf');
+var ejs = require('ejs');
 
-var parser = N3.Parser();
-var N3Util = N3.Util;
+var consts = require('./constants');
 
-// todo make configurable?
 
-var MODEL_NS_URI = 'http://ns.adobe.com/adobecloud/core/1.0/';
+function buildModel(graph) {
 
-// schema vocabulary
-
-var RDF_TYPE = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type';
-var RDFS_CLASS = 'http://www.w3.org/2000/01/rdf-schema#Class';
-var RDF_PROPERTY = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#Property';
-var RDFS_COMMENT = 'http://www.w3.org/2000/01/rdf-schema#comment';
-var RDFS_SUBCLASSOF = 'http://www.w3.org/2000/01/rdf-schema#subClassOf';
-var RDFS_ISDEFINEDBY = 'http://www.w3.org/2000/01/rdf-schema#isDefinedBy';
-var RDFS_DOMAIN = 'http://www.w3.org/2000/01/rdf-schema#domain';
-var RDFS_RANGE = 'http://www.w3.org/2000/01/rdf-schema#range';
-var RDFS_LABEL = 'http://www.w3.org/2000/01/rdf-schema#label';
-var RDFS_DATATYPE = 'http://www.w3.org/2000/01/rdf-schema#Datatype';
-var RDFS_RESOURCE = 'http://www.w3.org/2000/01/rdf-schema#Resource';
-
-var SKOS_EXAMPLE = 'http://www.w3.org/2004/02/skos/core#example';
-var SKOS_NOTE = 'http://www.w3.org/2004/02/skos/core#note';
-
-var META_JSONNAME = 'http://ns.adobe.com/adobecloud/meta/1.0/jsonName';
-
-var XSD_STRING = 'http://www.w3.org/2001/XMLSchema#string';
-var XSD_DATETINE = 'http://www.w3.org/2001/XMLSchema#dateTime';
-var XSD_FLOAT = 'http://www.w3.org/2001/XMLSchema#float';
-
-var ID = '@id'; // IRI of 'this' js object
-
-var triples = [];
-var model = { classes: {}, properties: {}, dataTypes: {} };
-
-var rdfStream = fs.createReadStream('./schemas/core.n3');
-
-function buildModel(triples) {
-
-  // TODO fix very inefficient and redundant processing of triples
+  // TODO use rdf Graph API instead of inefficient and redundant iterating over triples
+  var triples = graph.toArray();
+  // normalize to string values
+  triples = _.map(triples, function (triple) {
+    return {
+      subject: triple.subject.toString(),
+      predicate: triple.predicate.toString(),
+      object: triple.object.toString()
+    };
+  });
 
   // transform triples into objects ('subject') with attributes ('predicate') and attribute values ('object')
+
   var subjects = {};
   _.forEach(triples, function (triple) {
     var obj = subjects[triple.subject];
     if (!obj) {
       // inject '@id' attribute with object's IRI
       obj = subjects[triple.subject] = {};
-      obj[ID] = triple.subject;
+      obj[consts.ID] = triple.subject;
     }
     if (!obj[triple.predicate]) {
       obj[triple.predicate] = triple.object;
@@ -83,64 +60,142 @@ function buildModel(triples) {
       }
     }
   });
-  console.log(subjects);
+
+  // extract schema meta information
+  var meta;
+  _.forEach(subjects, function (subj, id) {
+    if (subj[consts.RDF_TYPE] === consts.OWL_ONTOLOGY) {
+      meta = subj;
+    }
+  });
 
   // extract our 'classes'
 
   var classes = {};
   _.forEach(subjects, function (subj, id) {
-    if (subj[RDF_TYPE] === RDFS_CLASS) {
-      if (N3Util.isIRI(id) && _.startsWith(id, MODEL_NS_URI)) {
-        // only consider classes defined in the specified NS
-        classes[id] = subj;
-      }
+    if (subj[consts.RDF_TYPE] === consts.RDFS_CLASS && subj[consts.RDFS_SUBCLASSOF] === consts.CORE_BASECLASS) {
+      classes[id] = subj;
     }
   });
-  _.forEach(subjects, function (subj, id) {
-    if (subj[RDFS_SUBCLASSOF] && subjects[subj[RDFS_SUBCLASSOF]]) {
-      if (N3Util.isIRI(id) && _.startsWith(id, MODEL_NS_URI)) {
-        // only consider classes defined in the specified NS
-        classes[id] = subj;
+  var derivedClasses;
+  do {
+    derivedClasses = {};
+    _.forEach(subjects, function (subj, id) {
+      if (!classes[id] && subj[consts.RDF_TYPE] === consts.RDFS_CLASS && subj[consts.RDFS_SUBCLASSOF] && classes[subj[consts.RDFS_SUBCLASSOF]]) {
+        derivedClasses[id] = subj;
       }
-    }
-  });
+    });
+  } while (Object.keys(derivedClasses).length && _.assign(classes, derivedClasses));
 
   // extract properties and attach to classes
 
   var properties = {};
   _.forEach(subjects, function (subj, id) {
-    if (subj[RDF_TYPE] === RDF_PROPERTY || (subjects[subj[RDF_TYPE]] && subjects[subj[RDF_TYPE]][RDFS_SUBCLASSOF] === RDF_PROPERTY)) {
-      if (subj[RDFS_DOMAIN] && classes[subj[RDFS_DOMAIN]]) {
+    if (subj[consts.RDF_TYPE] === consts.RDF_PROPERTY || (subjects[subj[consts.RDF_TYPE]] && subjects[subj[consts.RDF_TYPE]][consts.RDFS_SUBCLASSOF] === consts.RDF_PROPERTY)) {
+      if (subj[consts.RDFS_DOMAIN] && classes[subj[consts.RDFS_DOMAIN]]) {
         properties[id] = subj;
         // attach to class
-        var classProps = classes[subj[RDFS_DOMAIN]].properties;
+        var classProps = classes[subj[consts.RDFS_DOMAIN]].properties;
         if (!classProps) {
-          classProps = classes[subj[RDFS_DOMAIN]].properties = {};
+          classProps = classes[subj[consts.RDFS_DOMAIN]].properties = {};
         }
         classProps[id] = subj;
       }
     }
   });
 
-  model.classes = classes;
-  model.properties = properties;
+  // extract data types used by properties
 
-  console.log(model);
+  var dataTypes = {};
+  _.forEach(properties, function (subj, id) {
+    var typeId = subj[consts.RDFS_RANGE];
+    if (typeId && subjects[typeId]) {
+      dataTypes[typeId] = subjects[typeId];
+    }
+  });
+
+  return {
+    meta: meta,
+    classes: classes,
+    properties: properties,
+    dataTypes: dataTypes
+  };
 }
 
-parser.parse(rdfStream,
-  function (err, triple) {
-    if (err) {
-      console.log(err);
-    } else if (triple) {
-      triples.push(triple);
-      console.log(triple);
-    } else {
-      // EOF
-      buildModel(triples);
-    }
-  },
-  function (prefix, uri) {
-    console.log(prefix, uri);
+function parseN3(path, cb) {
+
+  var env = rdf.environment;
+  var graph = env.createGraph();
+  var turtleParser = new rdf.TurtleParser();
+
+  if (!turtleParser.parse(fs.readFileSync(path), undefined, null, null, graph)) {
+    cb(new Error('Parsing failed'));
+  } else {
+    cb(null, graph);
   }
-);
+}
+
+function prepareModel(model) {
+/*
+  var env = rdf.environment;
+
+  // map vocab uris to CURIE format
+
+  // TODO FIXME prefix mappings in parsed doc are not automatically added to env.prefixes
+  env.prefixes.set('core', 'http://ns.adobe.com/adobecloud/core/1.0/');
+  env.prefixes.set('meta', 'http://ns.adobe.com/adobecloud/meta/1.0/');
+  env.prefixes.set('xmp', 'http://ns.adobe.com/xap/1.0/');
+  env.prefixes.set('xmpMM', 'http://ns.adobe.com/xap/1.0/mm/');
+  env.prefixes.set('xmpRights', 'http://ns.adobe.com/xap/1.0/rights/');
+  env.prefixes.set('stDim', 'ttp://ns.adobe.com/xap/1.0/sType/Dimensions#');
+
+  var result = {};
+
+  function renameToCURIE(value, key) {
+    return env.prefixes.shrink(key);
+  }
+
+  result.meta = _.mapKeys(model.meta, renameToCURIE);
+  result.classes = {};
+  _.forEach(model.classes, function (value, key) {
+    result.classes[env.prefixes.shrink(key)] = _.mapKeys(value, renameToCURIE);
+  });
+  result.dataTypes = {};
+  _.forEach(model.dataTypes, function (value, key) {
+    result.dataTypes[env.prefixes.shrink(key)] = _.mapKeys(value, renameToCURIE);
+  });
+
+  return result;
+*/
+  console.log(model);
+  return model;
+}
+
+function renderModel(model) {
+  var ctx = {
+    model: model,
+    consts: consts,
+    _: _
+  };
+
+  ejs.renderFile('./templates/main.ejs', ctx, function (err, str) {
+    if (err) {
+      console.error(err);
+    } else {
+      if (!fs.existsSync('./out')) {
+        fs.mkdirSync('./out');
+      }
+      fs.writeFileSync('./out/main.html', str);
+
+      console.log(str);
+    }
+  });
+}
+
+parseN3('./schemas/core.n3', function (err, graph) {
+  if (err) {
+    console.log(err);
+  } else {
+    renderModel(prepareModel(buildModel(graph)));
+  }
+});
