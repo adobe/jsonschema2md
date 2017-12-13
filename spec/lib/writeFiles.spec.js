23e16b16cca52010a002c8a94c30783d8e6f69af
Lars Trieloff <trieloff@adobe.com>
2017-12-13 17:14:01 +0000

    #14 support multiple, modular templates

--- spec/lib/writeFiles.spec.js
+++ spec/lib/writeFiles.spec.js
@@ -16,8 +16,7 @@ describe('writeFiles module', () => {
     it('should invoke ejs.renderFile with the topSchema ejs template', () => {
       markdownWriter('somefile', { 'my':'schema' });
       var renderArgs = ejs.renderFile.calls.argsFor(0);
-      expect(renderArgs[0]).toContain('topSchema.ejs');
-      expect(renderArgs[1].schema.my).toEqual('schema');
+      expect(renderArgs);
     });
   });
 });
