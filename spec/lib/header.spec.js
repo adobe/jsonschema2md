4c422345e88e36a2335fce6869e11e5c994d8947
Lars Trieloff <trieloff@adobe.com>
2018-04-10 16:19:13 +0000

    generate full table

--- spec/lib/header.spec.js
+++ spec/lib/header.spec.js
@@ -1,23 +1,46 @@
-const { header, headers } = require('../../lib/header');
+const { Header, headers } = require('../../lib/header');
 
 describe('Header Integration Test', () => {
   it('Renders a header without link if link is unknown', () => {
-    const h = header('Foo');
+    const h = new Header('Foo');
     expect(h.renderHeader()).toEqual('Foo');
   });
 
   it('Renders a header with link if link is known', () => {
-    const h = header('Foo', '../bar.md');
+    const h = new Header('Foo', '../bar.md');
     expect(h.renderHeader()).toEqual('[Foo](../bar.md)');
   });
 
   it('Renders a header body without link if link is unknown', () => {
-    const h = header('Foo', null, 'Bar');
+    const h = new Header('Foo', null, 'Bar');
     expect(h.renderValue()).toEqual('Bar');
   });
 
   it('Renders a header body with link if link is known', () => {
-    const h = header('Foo', '../bar.md', 'Bar', '#bar-md');
+    const h = new Header('Foo', '../bar.md', 'Bar', '#bar-md');
     expect(h.renderValue()).toEqual('[Bar](#bar-md)');
   });
+});
+
+describe('Headers Integration Test', () => {
+  it('Abstract', () => {
+    const schema = {
+      additionalProperties: true
+    };
+    const props = {
+      abstract: 'Can be instantiated',
+      extensible: 'No',
+      status: 'Experimental',
+      custom: 'Forbidden',
+      original: 'complex.schema.json'
+    };
+
+    const h = headers(schema, props);
+
+    const result = `| Abstract | Extensible | Status | Custom Properties | Additional Properties | Defined In |
+|----------|------------|--------|-------------------|-----------------------|------------|
+| Can be instantiated | No | Experimental | Forbidden | Permitted | [complex.schema.json](complex.schema.json) |`;
+    expect(h.render()).toEqual(result);
+  });
+
 });
