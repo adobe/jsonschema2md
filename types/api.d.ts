44302e2dcff78e7e4b2770db24e55c71549edac1
Eduardo Rodrigues <eduardomourar@users.noreply.github.com>
2021-07-12 23:42:09 +0200

    chore(typescript): refactor public api declaration

--- types/api.d.ts
+++ types/api.d.ts
@@ -1,4 +1,4 @@
-/*
+/**
  * Copyright 2021 Adobe. All rights reserved.
  * This file is licensed to you under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License. You may obtain a copy
@@ -9,16 +9,15 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
+
 import { JSONSchema4, JSONSchema6, JSONSchema7 } from "json-schema";
 import { Root } from "mdast";
-import { filename, inputpath, outputcontent, outputpath } from "../lib/symbols";
+import { filename, fullpath } from "../lib/symbols";
 
 export type UniqueSymbols = {
     readonly pointer: unique symbol;
     readonly filename: unique symbol;
-    readonly inputpath: unique symbol;
-    readonly outputcontent: unique symbol;
-    readonly outputpath: unique symbol;
+    readonly fullpath: unique symbol;
     readonly id: unique symbol;
     readonly titles: unique symbol;
     readonly resolve: unique symbol;
@@ -28,23 +27,30 @@ export type UniqueSymbols = {
 };
 
 export type JsonSchema = JSONSchema4 | JSONSchema6 | JSONSchema7;
-export type SchemaFiles = {
-    [name: string]: string | JsonSchema;
+export type SchemaList = {
+    fileName: string;
+    fullPath: string;
 };
+export type SchemaContent = {
+    fileName: string;
+    fullPath?: string;
+    content: JsonSchema;
+};
+export type SchemaFiles = (SchemaList | SchemaContent)[];
 export type ExtendedJsonSchema = JsonSchema & {
     [filename]: string;
-    [inputpath]?: string;
-    [outputpath]?: string;
+    [fullpath]?: string;
 };
 
 export type MarkdownAst = Root;
-export type ExtendedMarkdownAst = MarkdownAst & {
-    [filename]: string;
-    [outputcontent]: string;
-    [outputpath]?: string;
+export type MarkdownContent = {
+    fileName: string;
+    fullPath: string;
+    markdownAst?: MarkdownAst;
+    content?: string;
 };
-export type ReadmeMarkdownAst = ExtendedMarkdownAst & {
-    [filename]: "README.md";
+export type ReadmeContent = MarkdownContent & {
+    fileName: "README.md";
 };
 
 /**
@@ -54,13 +60,13 @@ export type GeneratedOutput = {
     /**
      * Input JSON schemas
      */
-    schema: ExtendedJsonSchema[];
+    schema: SchemaContent[];
     /**
      * Ouput Readme file
      */
-    readme: ReadmeMarkdownAst;
+    readme?: ReadmeContent;
     /**
      * Output markdown data
      */
-    markdown: ExtendedMarkdownAst[];
+    markdown: MarkdownContent[];
 };
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,6 +1,17 @@
 declare module "symbols" {
     export = symbols;
     /**
+     * Copyright 2021 Adobe. All rights reserved.
+     * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+     * you may not use this file except in compliance with the License. You may obtain a copy
+     * of the License at http://www.apache.org/licenses/LICENSE-2.0
+     *
+     * Unless required by applicable law or agreed to in writing, software distributed under
+     * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+     * OF ANY KIND, either express or implied. See the License for the specific language
+     * governing permissions and limitations under the License.
+     */
+    /**
      * @typedef {import("../types/api").UniqueSymbols} UniqueSymbols
      */
     /**
@@ -41,24 +52,20 @@ declare module "markdownBuilder" {
     }): (schemas: any) => any;
 }
 declare module "writeMarkdown" {
-    export type JsonSchema = import("../types/api").JsonSchema;
     export type MarkdownAst = import("../types/api").MarkdownAst;
-    export type ExtendedMarkdownAst = import("../types/api").ExtendedMarkdownAst;
-    export type ReadmeMarkdownAst = import("../types/api").ReadmeMarkdownAst;
+    export type MarkdownContent = import("../types/api").MarkdownContent;
+    export type ReadmeContent = import("../types/api").ReadmeContent;
     export type MarkdownAstFiles = {
         [name: string]: import("mdast").Root;
     };
     /**
-     * @typedef {import("../types/api").JsonSchema} JsonSchema
-     */
-    /**
      * @typedef {import("../types/api").MarkdownAst} MarkdownAst
      */
     /**
-     * @typedef {import("../types/api").ExtendedMarkdownAst} ExtendedMarkdownAst
+     * @typedef {import("../types/api").MarkdownContent} MarkdownContent
      */
     /**
-     * @typedef {import("../types/api").ReadmeMarkdownAst} ReadmeMarkdownAst
+     * @typedef {import("../types/api").ReadmeContent} ReadmeContent
      */
     /**
      * @typedef {{ [name: string]: MarkdownAst }} MarkdownAstFiles
@@ -71,7 +78,7 @@ declare module "writeMarkdown" {
      * @param {Function} [options.info] - (optional) Info logger
      * @param {Function} [options.debug] - (optional) Debug logger
      * @param {{ [key: string]: string }} [options.meta] - (optional) Metadata to be added to Markdown
-     * @returns {(astFiles: MarkdownAstFiles) => ExtendedMarkdownAst[]}
+     * @returns {(astFiles: MarkdownAstFiles) => MarkdownContent[]}
      */
     export function writeMarkdown({ out, error, meta, }: {
         out?: string;
@@ -81,7 +88,7 @@ declare module "writeMarkdown" {
         meta?: {
             [key: string]: string;
         };
-    }): (astFiles: MarkdownAstFiles) => ExtendedMarkdownAst[];
+    }): (astFiles: MarkdownAstFiles) => MarkdownContent[];
     /**
      * Write the Readme Markdown to filesystem or an object
      * @param {Object} options
@@ -90,7 +97,7 @@ declare module "writeMarkdown" {
      * @param {Function} [options.info] - (optional) Info logger
      * @param {Function} [options.debug] - (optional) Debug logger
      * @param {{ [key: string]: string }} [options.meta] - (optional) Metadata to be added to Markdown
-     * @returns {(markdownAst: MarkdownAst) => ReadmeMarkdownAst}
+     * @returns {(markdownAst: MarkdownAst) => ReadmeContent}
      */
     export function writeReadme({ out, error, info, meta, }: {
         out?: string;
@@ -100,7 +107,7 @@ declare module "writeMarkdown" {
         meta?: {
             [key: string]: string;
         };
-    }): (markdownAst: MarkdownAst) => ReadmeMarkdownAst;
+    }): (markdownAst: MarkdownAst) => ReadmeContent;
 }
 declare module "readmeBuilder" {
     export = build;
@@ -135,45 +142,53 @@ declare module "schemaProxy" {
     const _exports: {
         pointer: unique symbol;
         filename: unique symbol;
-        inputpath: unique symbol;
-        outputcontent: unique symbol;
-        outputpath: unique symbol;
+        fullpath: unique symbol;
         id: unique symbol;
         titles: unique symbol;
         resolve: unique symbol;
         slug: unique symbol;
         meta: unique symbol;
         parent: unique symbol;
-        loader: () => (name: any, schema: any) => any;
+        loader: () => (name: string, schema: any) => any;
     };
     export = _exports;
-    export function loader(): (name: any, schema: any) => any;
+    export function loader(): (name: string, schema: any) => any;
 }
 declare module "writeSchema" {
-    export type JsonSchema = import("../types/api").JsonSchema;
     export type ExtendedJsonSchema = import("../types/api").ExtendedJsonSchema;
-    /**
-     * @typedef {import("../types/api").JsonSchema} JsonSchema
-     */
+    export type SchemaContent = import("../types/api").SchemaContent;
     /**
      * @typedef {import("../types/api").ExtendedJsonSchema} ExtendedJsonSchema
+     */
+    /**
+     * @typedef {import("../types/api").SchemaContent} SchemaContent
      */
     /**
      * Write the JSON Schemas to filesystem or an object
      * @param {Object} options
      * @param {string} [options.schemaDir] - (optional) Directory where the files will be saved
-     * @returns {(schemas: JsonSchema[]) => ExtendedJsonSchema[]}
+     * @param {string} [options.originDir] - (optional) Directory where the files were loaded from
+     * @returns {(schemas: ExtendedJsonSchema[]) => SchemaContent[]}
      */
-    export function writeSchema({ schemaDir }: {
+    export function writeSchema({ schemaDir, originDir }: {
         schemaDir?: string;
-    }): (schemas: JsonSchema[]) => ExtendedJsonSchema[];
+        originDir?: string;
+    }): (schemas: ExtendedJsonSchema[]) => SchemaContent[];
 }
 declare module "index" {
     export type JsonSchema = import("../types/api").JsonSchema;
+    export type SchemaList = import("../types/api").SchemaList;
+    export type SchemaContent = import("../types/api").SchemaContent;
     export type SchemaFiles = import("../types/api").SchemaFiles;
     export type GeneratedOutput = import("../types/api").GeneratedOutput;
     /**
      * @typedef {import("../types/api").JsonSchema} JsonSchema
+     */
+    /**
+     * @typedef {import("../types/api").SchemaList} SchemaList
+     */
+    /**
+     * @typedef {import("../types/api").SchemaContent} SchemaContent
      */
     /**
      * @typedef {import("../types/api").SchemaFiles} SchemaFiles
@@ -210,7 +225,7 @@ declare module "index" {
      * skip in markdown.
      * @returns {GeneratedOutput} List of raw markdown that were generated from input schema.
      */
-    export function jsonschema2md(schema: JsonSchema | SchemaFiles, { schemaPath, outDir, metadata, schemaOut, includeReadme, links, i18n, language, exampleFormat, includeProperties, header, skipProperties, }: {
+    export function jsonschema2md(schema: JsonSchema | import("../types/api").SchemaFiles, { schemaPath, outDir, metadata, schemaOut, includeReadme, links, i18n, language, exampleFormat, includeProperties, header, skipProperties, }: {
         schemaPath?: string;
         outDir?: string;
         metadata?: {
